e2996b9473747a5b2b8321403f6d02a76cc2b91a
Jasper van Rijbroek <jasper@sition.nl>
2020-04-10 22:21:02 +0200

    Added sitemap generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 module.exports = {
     siteMetadata: {
+        siteUrl: 'https://jvar.nl',
         title: `jvar.nl`,
         description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
         author: "@JaspervRijbro",
@@ -72,6 +73,7 @@ module.exports = {
                 }
             }
         },
+        `gatsby-plugin-sitemap`,
         `gatsby-plugin-offline`,
         `gatsby-plugin-sass`
     ],
